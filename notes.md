4992d86d6e273507eec51227b69c3bd3e52ffc18
Manfrinne <m4nfrinne@gmail.com>
2022-08-07 21:33:09 -0300

    add, checked and remove tasks

---
+++ notes.md
@@ -0,0 +1,13 @@
+# Projeto com React
+
+> doc: "https://reactjs.org/docs/create-a-new-react-app.html"
+
+## Iniciar projeto
+
+- npx create-react-app my-app
+- cd my-app
+- npm start
+
+## Pesquisar depois:
+
+- Como acessar funções de qualquer componente?
---
+++ src/App.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+import Tasks from "./componentes/Tasks";
+import AddTask from "./componentes/AddTask";
+
+import "./App.css";
+
+const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: "1",
+      title: "Estudar Programação",
+      completed: false,
+    },
+    {
+      id: "2",
+      title: "Ler Livros",
+      completed: true,
+    },
+  ]);
+
+  const handleTaskAddition = (taskTitle) => {
+    const newTasks = [
+      ...tasks,
+      {
+        id: uuidv4(),
+        title: taskTitle,
+        completed: false,
+      },
+    ];
+
+    setTasks(newTasks);
+  };
+
+  const handleTaskClick = (taskId) => {
+    const newTasks = tasks.map((task) => {
+      if (task.id === taskId) return { ...task, completed: !task.completed };
+
+      return task;
+    });
+
+    setTasks(newTasks);
+  };
+
+  const handleTaskDeletion = (taskId) => {
+    const newTasks = tasks.filter((task) => task.id !== taskId);
+
+    setTasks(newTasks);
+  };
+
+  return (
+    <>
+      <div className="container">
+        <AddTask handleTaskAddition={handleTaskAddition} />
+        <Tasks
+          tasks={tasks}
+          handleTaskClick={handleTaskClick}
+          handleTaskDeletion={handleTaskDeletion}
+        />
+      </div>
+      ;
+    </>
+  );
+};
+
+export default App;
